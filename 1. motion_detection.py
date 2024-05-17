fc8c7114cef49391837a2bc3b8e680e632ff5bec
Fabrício Giordanelli <fabricio.giordanelli@logcomex.com>
2024-05-17 08:37:07 -0300

    commit

---
+++ 1. motion_detection.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import time
+import actions
+
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+    _, frame1 = cap.read()
+    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.GaussianBlur(gray1, (21, 21), 0)
+
+    last_capture_time = time.time()
+    capture_interval = 20  # Interval in seconds
+
+    while True:
+        time.sleep(0.1)  # Limit frame rate
+        _, frame2 = cap.read()
+        gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.GaussianBlur(gray2, (21, 21), 0)
+
+        delta_frame = cv2.absdiff(gray1, gray2)
+        thresh_frame = cv2.threshold(delta_frame, 25, 255, cv2.THRESH_BINARY)[1]
+        thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+        contours, _ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        motion_detected = False
+        for contour in contours:
+            if cv2.contourArea(contour) < 1000:
+                continue
+            motion_detected = True
+            (x, y, w, h) = cv2.boundingRect(contour)
+            cv2.rectangle(frame2, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+        if motion_detected and (time.time() - last_capture_time > capture_interval):
+            actions.handle_detection(frame2)
+            last_capture_time = time.time()  # Update the last capture time
+
+        gray1 = gray2.copy()
+        cv2.imshow("Motion Detection", frame2)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ 2. teachable_machine.py
@@ -0,0 +1,32 @@
+import cv2
+from google.cloud import storage
+import time
+
+if person_detected and not photo_taken:
+    # Inicializa o cliente do Google Cloud Storage
+    filename = f"detected_{int(time.time())}.jpg"  # Salva com um timestamp para evitar sobreposições
+    cv2.imwrite(filename, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))         
+
+
+    client = storage.Client.from_service_account_json('sitio-421517-cbcc42238a47.json')
+
+    # Nome do seu bucket no Google Cloud Storage
+    bucket_name = 'people_detected'
+    bucket = client.get_bucket(bucket_name)
+
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(filename)  # Faz o upload para o GCS
+    url = f"https://storage.googleapis.com/{bucket_name}/{filename}"  # Retorna a URL pública
+    print(url)
+
+    account_sid = ''  # Use o SID da sua conta real para produção
+    auth_token = ''  # Use o token real da sua conta para produção
+    client = Client(account_sid, auth_token)
+    
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',  # Número do WhatsApp Sandbox do Twilio
+        body='Alerta',
+        media_url=[url],  # URL do arquivo local
+        to='whatsapp:+5511956028279'  # Seu número de WhatsApp com código do país
+    )
+    photo_taken = True
